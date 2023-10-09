fc97f56f933e43fd991df6585759d3349e2b21e2
Mate <mate@Ubuntu.myguest.virtualbox.org>
2023-10-09 18:19:36 +0200

    Initial commit, skeleton for project

---
+++ makefile
@@ -0,0 +1,2 @@
+all: main.c utils.c
+	gcc -o my_program main.c utils.c
---
+++ taxalpha.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    // Asks user for monthly income, stores value in income
+    cout << "What is your monthly income? ";
+    int income;
+    cin >> income;
+
+    // Asks user whether he is taxed progressively or flat
+    cout << "Type p for progressive, f for flat tax method";
+    string method;
+    cin >> method;
+}
