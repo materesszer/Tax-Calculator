5732c78baf37000caceca271a06cb90800ded1c8
Mate <mate@Ubuntu.myguest.virtualbox.org>
2023-10-09 19:17:24 +0200

    Organized program into functions

--- taxalpha.cpp
+++ taxalpha.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
-#include <string.h>
-using namespace std;
+
+int income(void);
+char method(void);
 
 int main()
 {
-    // Asks user for monthly income, stores value in income
-    cout << "What is your monthly income? ";
-    int income;
-    cin >> income;
+    int your_income = income();
+    char tax_method = method();
 
-    // Asks user whether he is taxed progressively or flat
-    cout << "Type p for progressive, f for flat tax method";
-    string method;
-    cin >> method;
+    std::cout << your_income << std::endl;
+    std::cout << tax_method << std::endl;
 }
+
+// Asks user for monthly income, stores value in income
+int income(void)
+{
+    std::cout << "What is your monthly income? ";
+    int income;
+    std::cin >> income;
+    return income;
+}
+
+// Asks user whether he is taxed progressively or flat
+char method(void)
+{
+    std::cout << "Type p for progressive, f for flat tax method ";
+    char method;
+    std::cin >> method;
+    method = tolower(method);
+    return method;
+}
+
